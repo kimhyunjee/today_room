17b52fb17d2632190b0b70390dcac78c4771f260
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-01-24 23:40:06 +0900

    fire base setting

---
+++ src/firebase.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9w8dhyIy2UVgupasRCy4BC8Fm5hEOMRQ",
+  authDomain: "today-room.firebaseapp.com",
+  projectId: "today-room",
+  storageBucket: "today-room.appspot.com",
+  messagingSenderId: "742532638690",
+  appId: "1:742532638690:web:62618df3f5174086c43bbb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//export default app // test용 export
