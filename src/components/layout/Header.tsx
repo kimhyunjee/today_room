4c7bbed0b80e721b1e69c26e32dd3eb6557ac100
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-02-16 03:10:32 +0900

    폰트 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import StoolImg from "../../assets/images/stool.png";
 import Logo from "../../assets/images/simpleLogo.jpg";
+import { FaSearch, FaCartPlus } from "react-icons/fa";
+import { FaUser } from "react-icons/fa6";
 
 const Header = () => {
   return (
     <>
-      <header className="bg-green w-full h-16 sticky top-0 ">
+      <header className=" w-full h-16 sticky top-0 ">
         <div className="w-11/12 max-w-screen-lg h-full flex items-center justify-between mx-auto">
           <div className="w-16">
             <img src={Logo} alt="logo" />
@@ -16,9 +16,15 @@ const Header = () => {
           <p>Today Room</p>
           <nav className="navigation">
             <ul className="flex list-none ml-7">
-              <li>메뉴1</li>
-              <li>메뉴2</li>
-              <li>메뉴3</li>
+              <li className="mx-5">
+                <FaSearch />
+              </li>
+              <li>
+                <FaCartPlus />
+              </li>
+              <li className="mx-5">
+                <FaUser />
+              </li>
             </ul>
           </nav>
         </div>
