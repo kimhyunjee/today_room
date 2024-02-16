6806bd5297e8e1a084fcde50e0e248df437508ec
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-02-16 18:25:05 +0900

    productLayout css수정

--- src/components/product/ProductLayout.tsx
+++ src/components/product/ProductLayout.tsx
@@ -21,8 +21,6 @@ import { FaCartPlus } from "react-icons/fa6";
 
 import { useQuery } from "@tanstack/react-query";
 import useFetchProduct from "@/hooks/useFetchProduct";
-import { defaultMaxListeners } from "stream";
-
 
 interface ProductData {
   title: string;
@@ -32,49 +30,62 @@ interface ProductData {
   id: "";
 }
 
-const ProductLayout = ({ dataList }) => {
+const ProductLayout = ({ dataList }: { dataList: ProductData }) => {
   const [imgList, setImgList] = useState<string[]>([]);
-  const {
-    isLoading,
-    data: product,
-    isError,
-    error,
-  } = useQuery({ queryKey: ["getProduct"], queryFn: useFetchProduct });
-  console.log(dataList);
+  // const { isLoading, data, isError, error } = useQuery({
+  //   queryKey: ["getProduct"],
+  //   queryFn: useFetchProduct,
+  // });
+
+  useEffect(() => {
+    const flattenedImgList = Array.from(Object.values(dataList))
+      .filter((item) => item.img && Array.isArray(item.img))
+      .reduce((acc, item) => acc.concat(item.img), [] as string[]);
+
+    setImgList(flattenedImgList);
+  }, []);
 
   return (
     <>
-      {dataList.map((item) => (
-        <Card key={(item as ProductData).id} className="w-[240px] h-80">
-          <CardHeader>
-            <Carousel className="w-full max-w-xs">
-              <CarouselContent>
-                {imgList.map((src, index) => (
+      {dataList.map((item: ProductData) => (
+        <Card key={(item as ProductData).id} className="w-[240px] h-100 my-8">
+          <Carousel className="w-full max-w-xs">
+            <CarouselContent>
+              {item.img &&
+                Array.isArray(item.img) &&
+                item.img.map((src, index) => (
                   <CarouselItem key={index}>
-                    <div className="p-1">
-                      <Card>
-                        <CardContent className="flex aspect-square items-center justify-center p-6">
-                          <img src={src} alt={`img ${index}`} />
-                        </CardContent>
-                      </Card>
-                    </div>
+                    <Card className="border-0 shadow-none">
+                      <CardContent className="flex aspect-square items-center justify-center p-6 ">
+                        <div className="w-full h-full object-cover">
+                          <img
+                            src={src}
+                            alt={`img ${index}`}
+                            className="w-full h-full object-fit"
+                          />
+                        </div>
+                      </CardContent>
+                    </Card>
                   </CarouselItem>
                 ))}
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
+            </CarouselContent>
+
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+
+          <CardContent>
             <CardTitle>{(item as ProductData).title}</CardTitle>
             <CardDescription>
               {(item as ProductData).description}
             </CardDescription>
-          </CardHeader>
-          <CardContent>{(item as ProductData).price}</CardContent>
+            <CardDescription> {(item as ProductData).price}원</CardDescription>
+          </CardContent>
           <CardFooter className="flex justify-between">
             <Button variant="outline">
               <FaCartPlus />
             </Button>
-            <Button variant="outline">구매하기</Button>
+            <Button variant="outline">상세보기</Button>
           </CardFooter>
         </Card>
       ))}
