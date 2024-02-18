c2a34ee83cfc588377ee2633144132cec3d52b39
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-02-19 01:26:41 +0900

    대시보드 삭제기능

--- src/components/product/SellerDashboard.tsx
+++ src/components/product/SellerDashboard.tsx
@@ -6,15 +6,34 @@ import { db } from "@/lib/firebase/firebase.config";
 import { useMutation } from "@tanstack/react-query";
 import { QUERY_KEY } from "@/constant/queryKey";
 import { queryClient } from "@/App";
+import { Product } from "@/lib/firebase/types";
 
 const SellerDashboard = () => {
   const { dataList } = useFetchProduct();
   const { mutate } = useMutation({
     mutationFn: ({ productId }: { productId: string }) =>
       productDelete(productId),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: QUERY_KEY.PRODUCT.MAIN() });
+    onMutate: async ({ productId }) => {
+      await queryClient.cancelQueries({ queryKey: QUERY_KEY.PRODUCT.MAIN() });
+      const previousProduct = queryClient.getQueryData(
+        QUERY_KEY.PRODUCT.MAIN()
+      );
+      queryClient.setQueryData(
+        QUERY_KEY.PRODUCT.MAIN(),
+        (prevProduct: Product[]) =>
+          prevProduct.filter((product) => product.id !== productId)
+      );
+      return { previousProduct };
     },
+    onError: (err, _, context) => {
+      queryClient.setQueryData(
+        QUERY_KEY.PRODUCT.MAIN(),
+        context?.previousProduct
+      );
+    },
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({ queryKey: QUERY_KEY.PRODUCT.MAIN() });
+    // },
   });
 
   const productDelete = async (productId: string) => {
